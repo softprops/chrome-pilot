19637afeeb918ebfc283d39037fe8af55eb235a2
softprops <d.tangren@gmail.com>
2012-07-06 16:23:20 -0400

    rm extra noise

--- server/src/main/scala/countdown.scala
+++ server/src/main/scala/countdown.scala
@@ -3,6 +3,6 @@ package chrome
 class Countdown[T](from: Int)(f: => T) {
   import java.util.concurrent.atomic.AtomicInteger 
   private val ai  = new AtomicInteger(from)
-  def tick = if (ai.decrementAndGet == 0) f else println("%d tabs still open " format ai.get)
+  def tick = if (ai.decrementAndGet == 0) f
   def reset(to: Int) = ai.getAndSet(to)
 }
