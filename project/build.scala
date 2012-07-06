baf0afc1b85987143a0e5378fa5f1cd8466fa369
softprops <d.tangren@gmail.com>
2012-07-06 14:00:53 -0400

    make dependencies more clear

--- project/build.scala
+++ project/build.scala
@@ -2,8 +2,8 @@ import sbt._
 import Keys._
 
 object Build extends sbt.Build {
-  lazy val server = Project("server", file("server")) dependsOn(tubesocks)
-  lazy val pilot = Project("pilot", file("pilot")) dependsOn(server)
   lazy val tubesocks = uri(
     "git://github.com/softprops/tubesocks#5950b56d1d1ecbb6b7b85b60a6a3731ecdf2b4fc")
+  lazy val server = Project("server", file("server")) dependsOn(tubesocks)
+  lazy val pilot = Project("pilot", file("pilot")) dependsOn(server)
 }
